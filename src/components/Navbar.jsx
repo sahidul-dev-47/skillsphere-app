1c21a26b813fce49104bac5c5280d03095f9992d
saiful-100 <sahidulx47@gmail.com>
2026-04-30 10:31:47 +0600

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import image from '@/assests/logo.png'
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>this is a navbar</h2>
+
+
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={image}
+            alt="logo"
+            loading="eager"
+            width={30}
+            height={30}
+            className="object-cover h-auto w-auto"
+          />
+          <h3 className="font-black text-lg">SkillSphere</h3>
         </div>
-    );
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/courses"}>Courses</Link>
+          </li>
+          <li>
+            <Link href={"/ my-profile"}> My Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
